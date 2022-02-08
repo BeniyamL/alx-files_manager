526e936b8871265b02ecf3cac99ae9c0a954aca0
Beniyam Legesse <lbeniyam@gmail.com>
2022-02-08 07:07:29 -0800

    files manager from task 0-6

---
+++ utils/redis.js
@@ -0,0 +1,34 @@
+const redis = require('redis');
+const { promisify } = require('utils');
+
+class RedisClient {
+  constructor() {
+    this.clnt = redis.createClient();
+    this.getAsc = promisify(this.clnt.get).bind(this.clnt);
+
+    this.clnt.on('error', (err) => {
+      console.log(`${err}`);
+    });
+  }
+
+  isAlive() {
+    return this.clnt.connected;
+  }
+
+  async get(k) {
+    const v = await this.getAsc(k);
+    return v;
+  }
+
+  async set(k, v, d) {
+    this.clnt.set(k, v);
+    this.clnt.expire(k, d);
+  }
+
+  async del(k) {
+    this.clnt.del(k);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
